e3e98445ed554f3e60180584c48bde5b63886f15
jbchinonso <nwekejohnbosco@gmail.com>
2020-02-22 15:31:23 -0800

    mobile menu improved

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const menu = document.querySelector('.mobile-menu img')
 const navbar = document.querySelector('nav ul')
 
+menu.addEventListener('click',(e)=>{
+    navbar.classList.toggle('nav-class')
+    if(e.target !== menu) navbar.className = ""
+})
 
-window.onclick = function(event){
-    if(event.target === menu){
-        navbar.classList.toggle('nav-class')
-        }else{
-            navbar.classList.remove('nav-class')
-        }
+window.onclick = function(e){
+    if(e.target !== menu) navbar.className = ""
     
 }
  
-menu.addEventListener('click',(e)=>{
-    navbar.classList.toggle('nav-class')
-    if(e.target !== menu) navbar.classList.remove('nav-class')
-})
+
 

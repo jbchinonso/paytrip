f9b90d2a9d4b79c390b84a27cf6df742cdab1809
jbchinonso <nwekejohnbosco@gmail.com>
2020-02-22 15:13:46 -0800

    improved user experience

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const menu = document.querySelector('.mobile-menu img')
 const navbar = document.querySelector('nav ul')
 
-window.onclick = function(event){
-    if(event.target == menu){
-        if(navbar.style.display == 'none'){
-            navbar.style.display = 'grid'
-        }else{
-            navbar.style.display = 'none'
-        }
-    }else{
-        navbar.style.display = 'none'
-    }
-}
 
+window.onclick = function(event){
+    if(event.target === menu){
+        navbar.classList.toggle('nav-class')
+        }else{
+            navbar.classList.remove('nav-class')
+        }
+    
+}
+/** 
+menu.addEventListener('click',(e)=>{
+    navbar.classList.toggle('nav-class')
+    if(e.target !== menu) navbar.classList.remove('nav-class')
+})
+
+*/
